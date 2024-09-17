4530473c40a02f27736176bf0150d7ed187be192
Zianoz <Zianpakapol@gmail.com>
2024-09-17 13:46:57 +0200

    a

---
+++ NumberGame/NumberGame/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej välkommen till nummberspelet!");
+            Random random = new Random();
+            int number = random.Next(1, 20);
+            Console.WriteLine("Jag tänker på ett nummer. Gissa vilket det är");
+            int player = int.Parse(Console.ReadLine());
+
+            if (number == player)
+            {
+                Console.WriteLine("Wohoo! Du klarade det!");
+            }
+            else
+            {
+                Console.WriteLine("FEL!");
+            }
+
+
+
+
+
+
+        }
+    }
+}
